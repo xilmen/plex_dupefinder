673826f4f4efc67c398c51f5fec32757206d0519
xilmen <romain.scha@gmail.com>
2020-06-20 03:52:15 +0200

    Change def "delete"

--- plex_dupefinder.py
+++ plex_dupefinder.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
 import collections
 import logging
 import os
@@ -197,13 +197,11 @@ def get_media_info(item):
     return info
 
 
-def delete_item(show_key, media_id):
-    delete_url = urljoin(cfg.PLEX_SERVER, '%s/media/%d' % (show_key, media_id))
-    log.debug("Sending DELETE request to %r" % delete_url)
-    if requests.delete(delete_url, headers={'X-Plex-Token': cfg.PLEX_TOKEN}).status_code == 200:
-        print("\t\tDeleted media item: %r" % media_id)
-    else:
-        print("\t\tError deleting media item: %r" % media_id)
+def delete_item(media_id, path):
+    path = ''.join(path)
+    print("remove " + str(media_id))
+    print("remove " + path )
+    os.remove(path)
 
 
 ############################################################
@@ -421,7 +419,7 @@ if __name__ == "__main__":
                         write_decision(keeping=part_info)
                     else:
                         print("\tRemoving : %r" % media_id)
-                        delete_item(part_info['show_key'], media_id)
+                        delete_item(media_id, part_info['file'])
                         write_decision(removed=part_info)
                         time.sleep(2)
             elif keep_item.lower() == 's' or int(keep_item) == 0:
@@ -462,7 +460,7 @@ if __name__ == "__main__":
                         if should_skip(part_info['file']):
                             print("\tSkipping removal of this item as there is a match in SKIP_LIST")
                             continue
-                        delete_item(part_info['show_key'], media_id)
+                        delete_item(media_id, part_info['file'])
                         write_decision(removed=part_info)
                         time.sleep(2)
             else:
